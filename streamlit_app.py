881b3f4f908b2a7f41274eb479a486d53e03f7b4
Sara Allaparthi <roja12@gmail.com>
2025-05-05 17:51:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 if ROOT_DIR not in sys.path:
     sys.path.insert(0, ROOT_DIR)
 
-# Load environment variables
 load_dotenv()
 
 import streamlit as st
