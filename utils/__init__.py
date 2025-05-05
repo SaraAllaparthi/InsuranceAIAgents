a132486e80a3381bac701753325505a9ade46191
Sara Allaparthi <roja12@gmail.com>
2025-05-05 15:59:14 +0200

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,1 +0,0 @@
-# (can be empty; this file just tells Python that utils/ is a package)
--- utils/decision_engine.py
+++
@@ -1,4 +0,0 @@
-def evaluate_claim(damage_info, weather_ok):
-    if damage_info['type'] == 'rain_damage' and weather_ok:
-        return True, ""
-    return False, "No matching weather event found"
