94ae1930ece7b35eb8e306f83c788ccf87979144
Sara Allaparthi <roja12@gmail.com>
2025-05-05 15:35:06 +0200

    Create payments.py

---
+++ utils/payments.py
@@ -0,0 +1,12 @@
+import os, stripe
+stripe.api_key = os.getenv('STRIPE_API_KEY')
+
+def issue_refund(amount, claimant_email):
+    payment_intent = stripe.PaymentIntent.create(
+        amount=int(amount*100),
+        currency='eur',
+        receipt_email=claimant_email,
+        payment_method_types=['card'],
+    )
+    refund = stripe.Refund.create(payment_intent=payment_intent.id)
+    return refund.id
